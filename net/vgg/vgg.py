a0a4dac6ae989c7eb7973619c0040b7992557bf8
juniorliu95 <544590746@qq.com>
2018-05-24 13:51:21 +0800

    123

--- net/vgg/vgg.py
+++ net/vgg/vgg.py
@@ -219,8 +219,8 @@ def vgg_19(inputs,
     with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
                         outputs_collections=end_points_collection):
       # net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
-      net = slim.conv2d(inputs, 64, [3, 3], activation_fn=None, normalizer_fn=None, scope='conv1')
-      net = slim.conv2d(net, 64, [3, 3], activation_fn=None, normalizer_fn=None, scope='conv1_2')
+      net = slim.conv2d(inputs, 64, [3, 3], activation_fn=None, normalizer_fn=None, scope='conv1_0')
+      net = slim.conv2d(net, 64, [3, 3], activation_fn=None, normalizer_fn=None, scope='conv1_1')
       net = slim.max_pool2d(net, [2, 2], scope='pool1')
       net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
       net = slim.max_pool2d(net, [2, 2], scope='pool2')
